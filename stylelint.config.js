63c01ccbbac0d4b3d0177e39df01eea95c5364ad
Valeriya <funny_imlay@list.ru>
2026-07-21 18:10:40 +0300

    build: добавляет stylelint и форматирует css по правилам

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+export default {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
+  rules: {
+    'selector-class-pattern': null,
+    'scss/dollar-variable-empty-line-before': null,
+  },
+};
